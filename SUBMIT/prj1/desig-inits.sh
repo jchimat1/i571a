5a36f0f387a7c186ad5e8b4a97911cab973a6183
Janaki Ram Chimata <113200465+jchimat1@users.noreply.github.com>
2022-11-05 00:49:49 -0400

    Adding prj1 files to submit directory

---
+++ SUBMIT/prj1/desig-inits.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#sets dir to directory containing this script
+dir=/home/jchimat1/projects/cs571/projects/prj1/prj1-sol/
+
+#use $dir/ as prefix to run any programs in this dir
+#so that this script can be run from any directory.
+node $dir/prj1code.js
+
+
